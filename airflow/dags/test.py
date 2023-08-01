328148fb970d2c3dafaa44186b4a072ae479f8bf
Rocio de sebastian <r.sebastian.ochotorena@gmail.com>
2023-08-01 14:17:29 +0200

    Airflow - apartado 2)

--- airflow/dags/test.py
+++ airflow/dags/test.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
 
 
 # argumentos definidos por defecto
@@ -12,9 +13,19 @@ default_args = {
     }
 
 # 1) Define un DAG llamado "test" que se ejecute cada día a las 3:00 UTC con unos argumentos por defecto.
-dag = DAG(
+with DAG(
     'test',
     default_args=default_args,
     schedule_interval='0 3 * * *',  # expresión crontab todos los días a las 3:00 UTC
     catchup=False  # evita que se ejecuten tareas pasadas al iniciar el DAG
-    )
+    ) as dag:
+
+    # 2) Incluye las tareas start y end como DummyOperator y que end vaya detrás de start en el DAG.
+    # tarea start
+    start = DummyOperator(
+            task_id = 'start')
+    # tarea end
+    end = DummyOperator(
+            task_id = 'end')
+    # dependencias
+    start >> end
